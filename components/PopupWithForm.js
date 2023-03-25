832dcb66ba33658e288280774fbdd1ca8a6b21b9
Olga Gvozdeva <atlasink@mail.ru>
2023-03-25 18:41:39 +0300

    full ref index

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,29 +1,17 @@
 import Popup from "./Popup.js";
 
 class PopupWithForm extends Popup{
-    constructor(popupSelector, {handleFormSubmit}){
+    constructor(popupSelector){
         super(popupSelector);
-        this._handleFormSubmit = handleFormSubmit;
         this._form = popupSelector.querySelector('.popup__form');
-        this._inputList = this._form.querySelectorAll('.popup__input');
       }
-    
-      _getInputValues(){
-        this._valueOfInput = {}
-        this._inputList.forEach(input => {
-          this._valueOfInput[input.name] = input.value;
-        })
-        return this._valueOfInput
-      }
-  
+      
       setEventListeners(){
         super.setEventListeners();
         this._form.addEventListener('submit', (evt) => {
           evt.preventDefault();
-          this._handleFormSubmit(this._getInputValues());
       })}
       
-
       closePopup() {
         super.closePopup();
         this._form.reset();
--- pages/index.js
+++ pages/index.js
@@ -1,107 +1,23 @@
-import Card from "../components/Card.js"
 import FormValidator from "../components/FormValidator.js";
-import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js"
-import UserInfo from "../components/UserInfo.js";
-import PopupWithImage from "../components/PopupWithImage.js"
 import {
-  buttonNameChange,
-  buttonAddPlace,
-  popupName,
-  popupOccupation,
-  elements,
-  popupFullPhoto,
-  popupChangeName,
-  popupAddPlace,
-  initialCards,
-  createNewCardObject,
-  formValidatorPlaceObject,
-  personalDetails
+  feedbackFormButton,
+  PopupFeedbackForm,
+  formValidatorObject,
 } from '../utils/constants.js'
 
 
-export const formValidatorPlace = new FormValidator(formValidatorPlaceObject, popupAddPlace);
-export const formValidatorName = new FormValidator(formValidatorPlaceObject, popupChangeName);
+const formValidatorFeedbackForm = new FormValidator(formValidatorObject, PopupFeedbackForm);
+formValidatorFeedbackForm.enableValidation();
 
-formValidatorPlace.enableValidation();
-formValidatorName.enableValidation();
-
-
-const createPopupAddPlace = new PopupWithForm(popupAddPlace, 
-
-  {handleFormSubmit: (formData) => {
-
-    const title = { 
-        name: formData['popupPlaceName'], 
-        link: formData['popupPlaceLink'] 
-      } 
-      
-      document.querySelector(elements).prepend(createNewCard(title)); 
-  /*
-      const beta = document.querySelector('.element__photo');
-      beta.onerror = () => {
-        title.link = withoutImg;
-        console.log('nea')
-      }
-*/
-      createPopupAddPlace.closePopup();   
-    }});
-
-createPopupAddPlace.setEventListeners();
-
-const userDetails = new UserInfo(personalDetails);
-
-const createPopupProfileEdit = new PopupWithForm(popupChangeName, 
-  {handleFormSubmit: (formData) => {
-    userDetails.setUserInfo(formData['popup__content_type_name'], formData['popup__content_type_occupation']);
-    createPopupProfileEdit.closePopup();
-  }
-});
-
-createPopupProfileEdit.setEventListeners();
-
-const createPopupFullImg = new PopupWithImage(popupFullPhoto);
-createPopupFullImg.setEventListeners();
-
+const createPopupFeedbackForm = new PopupWithForm(PopupFeedbackForm);
+createPopupFeedbackForm.setEventListeners();
   
-function createNewCard(item){
-  const card = new Card(
-    item, 
-    '#element-template', 
-    createNewCardObject, 
-    {handleCardClick: (name, link) => {
-      createPopupFullImg.openPopup(name, link);
-    }});
-  const cardElement = card.generateCard();
-  return cardElement
+function PopupForm(){
+  createPopupFeedbackForm.openPopup();
+  formValidatorFeedbackForm.resetOpnForm();
 }
 
-const createCardStaticList = new Section({
-  items: initialCards,
-  renderer: (elem) => {
-    createNewCard(elem);
-    createCardStaticList.addItem(createNewCard(elem));
-  }
-}, elements);
+feedbackFormButton.addEventListener('click', PopupForm);
 
-createCardStaticList.renderItems();
-
-
-function openPopupAddPlace(){
-  createPopupAddPlace.openPopup();
-  formValidatorPlace.resetOpnForm();
-}
-
-
-function openPopupProfileEdit (){
-
-  popupName.value = userDetails.getUserInfo().name;
-  popupOccupation.value = userDetails.getUserInfo().occupation;
-
-  createPopupProfileEdit.openPopup();
-  formValidatorName.resetOpnForm();
-}
-
-buttonNameChange.addEventListener('click', openPopupProfileEdit);
-buttonAddPlace.addEventListener('click', openPopupAddPlace);
 
